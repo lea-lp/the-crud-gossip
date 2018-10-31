c2509c807160f129694df48065523cf54b911285
lea-lp <lea.leipp@gmail.com>
2018-10-31 19:17:48 +0100

    fin edit

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -8,9 +8,6 @@ class GossipsController < ApplicationController
       @new_gossip = Gossip.new
     end
 
-    def update
-    end
-
     def create
       new_gossip = Gossip.create(anonymous_gossiper: params[:gossip][:anonymous_gossiper], title: params[:gossip][:title], content: params[:gossip][:content])
       redirect_to(gossip_path(new_gossip.id))
@@ -21,5 +18,15 @@ class GossipsController < ApplicationController
       # @gossip = Gossip.find(params[:id])
     end
 
+    def edit
+      @gossip = Gossip.find(params[:id])
+    end
+
+    def update
+      @gossip = Gossip.find(params[:id])
+      gossip_params = params.require(:gossip).permit(:title, :content)
+      @gossip.update(gossip_params)
+      redirect_to gossips_path
+    end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
 
   resources :gossips
   root to: 'static_pages#home'
-  resources :static_pages
-
 end
